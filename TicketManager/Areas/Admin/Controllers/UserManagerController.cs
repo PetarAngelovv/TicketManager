6445a99bd875d1aa2f4a053d971cc4bf41c096f4
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-07-16 22:50:24 +0300

    Add UserManagementController and View to list users in Admin area

---
+++ TicketManager/Areas/Admin/Controllers/UserManagerController.cs
@@ -0,0 +1,50 @@
+﻿using GCommon;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TicketManager.Web.ViewModels.User;
+using static GCommon.GlobalValidation;
+
+namespace TicketManager.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = RoleConstants.Admin)]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserManagementController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUserId = userManager.GetUserId(User); 
+            var users = userManager.Users
+                                   .Where(u => u.Id != currentUserId)  
+                                   .ToList();
+
+            var model = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+
+                if (roles.Contains(RoleConstants.Admin))
+                {
+                    continue; 
+                }
+
+                model.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email!,
+                    IsManager = roles.Contains(RoleConstants.Manager)
+                });
+            }
+
+            return View(model);
+        }
+    }
+}

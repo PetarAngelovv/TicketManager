49eaa59cd12f89bc84e67f813f7dd90d7b19e952
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-07-16 23:20:12 +0300

    Implement Add/Remove Manager roles, add role management forms in UserManagement View

--- TicketManager/Areas/Admin/Controllers/AdminController.cs
+++ TicketManager/Areas/Admin/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using static GCommon.GlobalValidation;
 
 namespace TicketManager.Web.Areas.Admin.Controllers
 {
@@ -15,27 +16,29 @@ namespace TicketManager.Web.Areas.Admin.Controllers
         {
             this.userManager = userManager;
         }
-
+        [HttpPost]
         public async Task<IActionResult> AddManagerRole(string userId)
         {
             var user = await userManager.FindByIdAsync(userId);
-            if (user != null && !await userManager.IsInRoleAsync(user, "Manager"))
+            if (user != null && !await userManager.IsInRoleAsync(user, RoleConstants.Manager))
             {
                 // Добавя Manager роля на потребителя
-                await userManager.AddToRoleAsync(user, "Manager");
+                await userManager.AddToRoleAsync(user, RoleConstants.Manager);
             }
-            return RedirectToAction("Index", "UserManagement"); // или където искаш
-        }
+            return RedirectToAction("Index", "UserManagement", new { area = "Admin" });
 
+        }
+        [HttpPost]
         public async Task<IActionResult> RemoveManagerRole(string userId)
         {
             var user = await userManager.FindByIdAsync(userId);
-            if (user != null && await userManager.IsInRoleAsync(user, "Manager"))
+            if (user != null && await userManager.IsInRoleAsync(user, RoleConstants.Manager))
             {
                 // Премахва Manager роля от потребителя
-                await userManager.RemoveFromRoleAsync(user, "Manager");
+                await userManager.RemoveFromRoleAsync(user, RoleConstants.Manager);
             }
-            return RedirectToAction("Index", "UserManagement");
+            return RedirectToAction("Index", "UserManagement", new { area = "Admin" });
+
         }
     }
 

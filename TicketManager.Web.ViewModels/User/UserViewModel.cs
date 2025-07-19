e5e10423d687c629c2b0fb34032e36a611f9f198
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-07-19 19:29:11 +0300

    Adding UserViewModel for User Management

---
+++ TicketManager.Web.ViewModels/User/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManager.Web.ViewModels.User
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public bool IsManager { get; set; }
+    }
+}

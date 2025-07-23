8e3ea44d1193f5ddb42b8e917221cb5dbbb9be34
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-07-23 23:55:21 +0300

    removing useless "using"

--- TicketManager.Data/TicketManagerDbContext.cs
+++ TicketManager.Data/TicketManagerDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using RecipeSharingPlatform.Data.Configurations;
 using System.Reflection;
 using TicketManager.Data.Models;
 
